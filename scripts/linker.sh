022b4c325b1bb5483df70770edb0080590b42f2f
mitch <mitch.riley1772@gmail.com>
2021-05-22 18:59:58 +0000

    removed amazon because they suck

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -4,11 +4,11 @@
 
 #### AMAZON ####
 # update amazon store files add cart URL with AID number 
-find ./build/src/store/model/amazon*.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '/cartUrl/s/Quantity.1=1/&\&tag=randoprojects-20\&AssociateTag=randoprojects-20/'
+#find ./build/src/store/model/amazon*.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '/cartUrl/s/Quantity.1=1/&\&tag=randoprojects-20\&AssociateTag=randoprojects-20/'
 
 # update amazon store URL with AID number 
 # ex:( 'https://www.amazon.com/dp/B08P2HBBLX' -> 'https://www.amazon.com/dp/B08P2HBBLX?tag=randoprojects-20' )
-find ./build/src/store/model/amazon*.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '/url:/s/dp\/[^\/'\'']*/&?tag=randoprojects-20/'
+#find ./build/src/store/model/amazon*.js \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i '/url:/s/dp\/[^\/'\'']*/&?tag=randoprojects-20/'
 
 ### ASUS ###
 # update asus store URL with AID number 
